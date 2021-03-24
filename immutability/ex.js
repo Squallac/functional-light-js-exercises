3714cb37bb4ec610e4e0608ad583e55aea451528
RRuiz <raul.ruiz@care.com>
2021-03-24 12:35:33 -0600

    Adding immutability exercise

--- immutability/ex.js
+++ immutability/ex.js
@@ -1,15 +1,47 @@
 "use strict";
 
 function lotteryNum() {
-	return (Math.round(Math.random() * 100) % 58) + 1;
+  return (Math.round(Math.random() * 100) % 58) + 1;
 }
+function pickNumber(num, numbers) {
+  const existInLotteryNums = numbers.indexOf(num) !== -1;
 
-function pickNumber() {}
+  if (!existInLotteryNums) {
+    // numbers = numbers.slice();
+    // numbers.push(num);
+    numbers = [...numbers, num];
+
+    numbers.sort(function byAscNum(s1, s2) {
+      console.log(s1);
+      console.log(s2);
+      return s1 - s2;
+    });
+  }
+  return numbers;
+}
 
 var luckyLotteryNumbers = [];
 
-while (luckyLotteryNumbers.length < 6) {
-	pickNumber();
+function getOrderedLotteryNumbers() {
+  luckyLotteryNumbers = luckyLotteryNumbers.slice();
+  return sortLotteryNumbers();
+
+  function sortLotteryNumbers() {
+    luckyLotteryNumbers.sort(function byNum(s1, s2) {
+      console.log("prro");
+      console.log(s1);
+      console.log(s2);
+      return s1 < s2;
+    });
+    return luckyLotteryNumbers;
+  }
 }
 
-console.log(luckyLotteryNumbers);
+while (luckyLotteryNumbers.length < 6) {
+  luckyLotteryNumbers = pickNumber(
+    lotteryNum(),
+    Object.freeze(luckyLotteryNumbers)
+  );
+}
+
+console.log(getOrderedLotteryNumbers());
