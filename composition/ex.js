78f3eafe31191a2a9784f59bf9ec491b38b2a8bf
RRuiz <raul.ruiz@care.com>
2021-03-24 10:45:08 -0600

    Adding composition exercise

--- composition/ex.js
+++ composition/ex.js
@@ -1,23 +1,41 @@
 "use strict";
 
-function increment(x) { return x + 1; }
-function decrement(x) { return x - 1; }
-function double(x) { return x * 2; }
-function half(x) { return x / 2; }
+function increment(x) {
+  return x + 1;
+}
+function decrement(x) {
+  return x - 1;
+}
+function double(x) {
+  return x * 2;
+}
+function half(x) {
+  return x / 2;
+}
 
-function compose() { return compose; }
-function pipe() { return pipe; }
+function compose(...listedFunctions) {
+  return pipe(...listedFunctions.reverse());
+}
+function pipe(...listedFunctions) {
+  return function piping(value) {
+    return listedFunctions.reduce((accumulator, currentFunction) => {
+      console.log("accumulator", accumulator);
+      console.log("currentFunction", currentFunction);
+      return currentFunction(accumulator);
+    }, value);
+  };
+}
 
-var f1 = compose(increment,decrement);
-var f2 = pipe(decrement,increment);
-var f3 = compose(decrement,double,increment,half);
-var f4 = pipe(half,increment,double,decrement);
+var f1 = compose(increment, decrement);
+var f2 = pipe(decrement, increment);
+var f3 = compose(decrement, double, increment, half);
+var f4 = pipe(half, increment, double, decrement);
 var f5 = compose(increment);
 var f6 = pipe(increment);
 
-console.log( f1(3) === 3 );
-console.log( f1(3) === f2(3) );
-console.log( f3(3) === 4 );
-console.log( f3(3) === f4(3) );
-console.log( f5(3) === 4 );
-console.log( f5(3) === f6(3) );
+console.log(f1(3) === 3);
+console.log(f1(3) === f2(3));
+console.log(f3(3) === 4);
+console.log(f3(3) === f4(3));
+console.log(f5(3) === 4);
+console.log(f5(3) === f6(3));
