e6e0a85b132597e998e78c3fd1b206fe73229795
RRuiz <raul.ruiz@care.com>
2021-03-24 14:52:37 -0600

    Adding recursion exercise

--- recursion/ex.js
+++ recursion/ex.js
@@ -1,15 +1,44 @@
 "use strict";
 
-function isPalindrome() {}
+// abba
 
-console.log( isPalindrome("") === true );
-console.log( isPalindrome("a") === true );
-console.log( isPalindrome("aa") === true );
-console.log( isPalindrome("aba") === true );
-console.log( isPalindrome("abba") === true );
-console.log( isPalindrome("abccba") === true );
+// abccba
+// bccb
+// cc
+// ''
 
-console.log( isPalindrome("ab") === false );
-console.log( isPalindrome("abc") === false );
-console.log( isPalindrome("abca") === false );
-console.log( isPalindrome("abcdba") === false );
+function isPalindrome(str) {
+  // invalid cases, base condition
+  if (!str && str != "") {
+    return;
+  }
+
+  // special conditions
+  if (str.length === 1 || str === "") {
+    return true;
+  }
+
+  const firstChar = str[0];
+  const lastChar = str[str.length - 1];
+
+  if (firstChar !== lastChar) {
+    return false;
+  }
+
+  const substring = str.slice(1, str.length - 1);
+  console.log("substring", substring);
+
+  return isPalindrome(substring);
+}
+
+console.log(isPalindrome("") === true);
+console.log(isPalindrome("a") === true);
+console.log(isPalindrome("aa") === true);
+console.log(isPalindrome("aba") === true);
+console.log(isPalindrome("abba") === true);
+console.log(isPalindrome("abccba") === true);
+
+console.log(isPalindrome("ab") === false);
+console.log(isPalindrome("abc") === false);
+console.log(isPalindrome("abca") === false);
+console.log(isPalindrome("abcdba") === false);
