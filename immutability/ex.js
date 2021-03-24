ca4c90385793ed378047ff169a5d2db8e674553c
RRuiz <raul.ruiz@care.com>
2021-03-24 12:35:55 -0600

    Adding immutability exercise

--- immutability/ex.js
+++ immutability/ex.js
@@ -7,10 +7,8 @@ function pickNumber(num, numbers) {
   const existInLotteryNums = numbers.indexOf(num) !== -1;
 
   if (!existInLotteryNums) {
-    // numbers = numbers.slice();
-    // numbers.push(num);
-    numbers = [...numbers, num];
-
+    numbers = numbers.slice();
+    numbers.push(num);
     numbers.sort(function byAscNum(s1, s2) {
       console.log(s1);
       console.log(s2);
@@ -22,24 +20,9 @@ function pickNumber(num, numbers) {
 
 var luckyLotteryNumbers = [];
 
-function getOrderedLotteryNumbers() {
-  luckyLotteryNumbers = luckyLotteryNumbers.slice();
-  return sortLotteryNumbers();
-
-  function sortLotteryNumbers() {
-    luckyLotteryNumbers.sort(function byNum(s1, s2) {
-      console.log("prro");
-      console.log(s1);
-      console.log(s2);
-      return s1 < s2;
-    });
-    return luckyLotteryNumbers;
-  }
-}
-
 while (luckyLotteryNumbers.length < 6) {
   luckyLotteryNumbers = pickNumber(
-    lotteryNum(),
+    lotteryNum(), //extracting impurity here
     Object.freeze(luckyLotteryNumbers)
   );
 }
